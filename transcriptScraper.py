99638d25e9d3b75d852dc02e9dea525c7a3f7f7a
Michael Curry <mike@exodusdev.com>
2022-10-01 06:53:06 -0700

    Fixes for current CSPAN transcript structure
    Also flake8 reformatting

--- transcriptScraper.py
+++ transcriptScraper.py
@@ -13,44 +13,88 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import argparse
 
+import argparse
+import logging
 from datetime import timedelta
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def getTranscriptChunk(tds,browser):
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+def getTranscriptChunk(tds, browser):
     transcriptChunk = []
     for td in tds:
-        if td.get_attribute('class') == 'image':
+        if td.get_attribute("class") == "image":
             continue
+        a = None
         try:
-            a = td.find_element(By.CLASS_NAME, 'hidden-full-transcript-link')
-            browser.execute_script("arguments[0].click();",a)
-        except:
+            a = td.find_element(By.CLASS_NAME, "hidden-full-transcript-link")
+            browser.execute_script("arguments[0].click();", a)
+            logger.debug("wait for element to appear")
+            e = WebDriverWait(browser, 1).until(
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, "hidden-full-transcript-text")
+                )
+            )
+            logger.debug("element appeared within expected time")
+        except NoSuchElementException as ex:
+            logger.debug(".hidden-full-transcript-link element not found")
             pass
         # Following isn't quite correct; some transcript text is
         #  in elements without the stated class
-        chunks = td.find_elements(By.CLASS_NAME, 'transcript-time-seek')
-        for chunk in chunks:
-            if chunk.text.strip() == "":
-                continue
-            transcriptChunk.append(chunk.text)
-    return(" ".join(transcriptChunk))
+        if a:
+            # Has full transcript dropdown widget
+            logger.debug("found full transcript dropdown")
+        else:
+            logger.debug("short transcript only")
+        # Grab the short transcript <p> element and the text shall set you free
+        short_transcript = td.find_element(By.CLASS_NAME, "short_transcript")
+        logger.debug("short_transcript_text='%s'", short_transcript.text)
+        transcriptChunk.append(short_transcript.text.strip())
+
+        # chunks = td.find_elements(By.CLASS_NAME, "transcript-time-seek")
+        # if len(chunks):
+        #     logger.info("alt 1", chunks)
+        #     print("chunks", chunks)
+        #     for chunk in chunks:
+        #         print("\tchunk", chunk)
+        #         if chunk.text.strip() == "":
+        #             continue
+        #         transcriptChunk.append(chunk.text)
+        # else:
+        #     # alt 1
+        #     logger.info("alt 2")
+        #     try:
+        #         fullts = td.find_element(By.CLASS_NAME, "show-hidden-full-transcript")
+        #         print("fullts", fullts)
+        #     except NoSuchElementException:
+        #         logger.debug(".show-hidden-full-transcript not found ")
+        #         pass
+    return " ".join(transcriptChunk)
+
 
 def getTimestamp(row):
-    header = row.find_element(By.TAG_NAME, 'th')
-    stamp = [int(x) for x in header.text.split(':')]
-    t = timedelta(hours=stamp[0],minutes=stamp[1], seconds=stamp[2])
-    return(t.total_seconds())
+    header = row.find_element(By.TAG_NAME, "th")
+    stamp = [int(x) for x in header.text.split(":")]
+    t = timedelta(hours=stamp[0], minutes=stamp[1], seconds=stamp[2])
+    return t.total_seconds()
+
 
 def faveOutput(chunks, times):
     rows = []
     for i in range(len(chunks)):
-        row = ['?', '?', str(times[i]), str(times[i+1]), str(chunks[i])]
-        rows.append('\t'.join(row))
-    return('\n'.join(rows))
+        row = ["?", "?", str(times[i]), str(times[i + 1]), str(chunks[i])]
+        rows.append("\t".join(row))
+    return "\n".join(rows)
+
 
 def main(url, outName):
     ffops = webdriver.FirefoxOptions()
@@ -59,33 +103,35 @@ def main(url, outName):
 
     browser.get(url)
 
-    duration = browser.find_element(By.CLASS_NAME, 'jw-video-duration').text
-    dur = [int(x) for x in duration.split(':')]
-    duration = timedelta(hours=dur[0],minutes=dur[1],seconds=dur[2]).total_seconds()
+    duration = browser.find_element(By.CLASS_NAME, "jw-video-duration").text
+    dur = [int(x) for x in duration.split(":")]
+    duration = timedelta(hours=dur[0], minutes=dur[1], seconds=dur[2]).total_seconds()
 
-    sec = browser.find_element_by_class_name('transcript')
-    rows = sec.find_elements(By.TAG_NAME, 'tr')
+    sec = browser.find_element(By.CLASS_NAME, "transcript")
+    rows = sec.find_elements(By.TAG_NAME, "tr")
     transcript = []
     times = []
     for row in rows:
-        tds = row.find_elements(By.TAG_NAME, 'td')
-        chunk = getTranscriptChunk(tds,browser)
+        tds = row.find_elements(By.TAG_NAME, "td")
+        chunk = getTranscriptChunk(tds, browser)
         time = getTimestamp(row)
         transcript.append(chunk)
         times.append(time)
     browser.close()
     times.append(duration)
 
-    outText = faveOutput(transcript,times)
+    outText = faveOutput(transcript, times)
 
-    with open(outName, 'w') as f:
+    with open(outName, "w") as f:
         f.write(outText)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("url", help="The cspan url to get a transcript from.")
-    parser.add_argument("output", help="Name of file that transcript should be written to.")
+    parser.add_argument(
+        "output", help="Name of file that transcript should be written to."
+    )
     args = parser.parse_args()
     main(args.url, args.output)
-
